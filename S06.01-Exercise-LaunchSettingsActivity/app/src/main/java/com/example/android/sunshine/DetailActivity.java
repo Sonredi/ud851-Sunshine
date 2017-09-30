a3493978de3c513d158b93b4a1944d3e1881993d
jcruz <joscruzpe@gmail.com>
2017-09-29 20:18:42 -0500

    Add settings menu

--- S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -56,4 +56,17 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     // TODO (7) Launch SettingsActivity when the Settings option is clicked
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            Intent startSettingActivity = new Intent(this, SettingsActivity.class);
+            startActivity(startSettingActivity);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
